ad330d639f36922a0a8704bf343d8e555f983f4a
HoangTien339 <tiennh21@fpt.edu.vn>
2022-03-09 11:17:18 +0700

    Lecture 2: Servlet + JSP

--- src/main/java/controllers/LoginServlet.java
+++ src/main/java/controllers/LoginServlet.java
@@ -1,34 +1,36 @@
 package controllers;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/HelloServlet")
-public class HelloServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    public HelloServlet() {
-        super();
-    }
-
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
 	protected void doGet(
 		HttpServletRequest request,
 		HttpServletResponse response
 	) throws ServletException, IOException {
-		String name = request.getParameter("ho_ten");
-		request.setAttribute("name", name);
-		request.getRequestDispatcher("/views/welcome.jsp")
+		request.getRequestDispatcher("/views/login.jsp")
 			.forward(request, response);
 	}
-
+	
 	protected void doPost(
 		HttpServletRequest request,
 		HttpServletResponse response
-	) throws ServletException, IOException {
-		doGet(request, response);
+	) {
+		String email = request.getParameter("email"),
+			pwd = request.getParameter("password");
+
+		System.out.println(email + "-" + pwd);
+		if (
+			email.equalsIgnoreCase("tiennh21@fpt.edu.vn")
+			&& pwd.equals("123123")
+		) {
+			System.out.println("Đăng nhập thành công");
+		}
 	}
 }
